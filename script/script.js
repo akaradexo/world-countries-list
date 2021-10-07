998c3cde85b20b3411f3b456de4211a6503fd8f4
Ashutosh Karade <69238714+xR0v3Rx@users.noreply.github.com>
2021-10-07 18:05:13 +0530

    ui

--- script/script.js
+++ script/script.js
@@ -1,49 +1,66 @@
 'use strict'
-import { countries } from '../data/countries.js'
+const countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Antigua and Barbuda","Argentina","Armenia","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bhutan","Bolivia","Bosnia and Herzegovina","Botswana","Brazil","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Central African Republic","Chad","Chile","China","Colombi","Comoros","Congo (Brazzaville)","Congo","Costa Rica","Cote d","Ivoire","Croatia","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","East Timor (Timor Timur)","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Fiji","Finland","France","Gabon","Gambia, The","Georgia","Germany","Ghana","Greece","Grenada","Guatemala","Guinea","Guinea-Bissau","Guyana","Haiti","Honduras","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Israel","Italy","Jamaica","Japan","Jordan","Kazakhstan","Kenya","Kiribati","Korea, North","Korea, South","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Mauritania","Mauritius","Mexico","Micronesia","Moldova","Monaco","Mongolia","Morocco","Mozambique","Myanmar","Namibia","Nauru","Nepal","Netherlands","New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palau","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Qatar","Romania","Russia","Rwanda","Saint Kitts and Nevis","Saint Lucia","Saint Vincent","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia and Montenegro","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","Spain","Sri Lanka","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Togo","Tonga","Trinidad and Tobago","Tunisia","Turkey","Turkmenistan","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States","Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam","Yemen","Zambia","Zimbabwe"];
+
+
 //declare
-const topPopulation = document.getElementById("population")
-const topLanguage = document.getElementById("language")
-const btnPopulation = document.getElementById("btnPopulation")
-const btnLanguage = document.getElementById("btnLanguage")
-const populationName = document.getElementById("populationName")
-const populationChart = document.getElementById("populationChart")
-const populationNumber = document.getElementById("populationNumber")
+const output = document.getElementById("output");
+const wordSearch = document.getElementById("wordSearch");
+const anySearch = document.getElementById("anySearch");
+const filter = document.getElementById("filter");
+const search = document.getElementById("search");
 
-
-//user array
-let topPList=[]
-console.log(topPList.values())
-
-// console.log(xMax)
-
-
-//Adding user to users array
-// const addData = function (list) {
-//   topPList.push(list);
-//   updateDom();
-// };
-
-
-  let xValues = countries.map(function(o) { return o.population; });
-  let xMax = [...xValues].sort((a,b) => b-a).slice(0,10);
-  xMax.forEach((element,index) => {
-  const populationFiltered = countries.filter((function(o) { return o.population === xMax[index]; }))
-  topPList.push(element)
-  });
-
-
-
-
-const showPopulation = function(){
-  topPopulation.style.display="flex"
-  topLanguage.style.display="none"
+output.innerHTML = countries.map(myFunction)
+function myFunction(value) {
+  return(
+    `<div class="output"><h2>${value}</h2></div>`
+  );
 }
-const showLanguage = function(){
-  topPopulation.style.display="none"
-  topLanguage.style.display="flex"
+
+
+
+// let txt = "";
+// countries.forEach(myFunction);
+// output.innerHTML = txt;
+
+// function myFunction(value, index, array) {
+//   `<div class="output"><h2>${value}</h2></div>`; 
+// }
+
+
+
+
+const searchByWord  = function(){
+  console.log(searchByWord)
+console.log("sds")
+
 }
+const wordSearchName  = function(e){
+  e.preventDefault()
+console.log("sds")
+
+
+}
+const anySearchName  = function(e){
+  e.preventDefault()
+console.log("sds")
+
+  
+}
+const filterName  = function(e){
+  e.preventDefault()
+
+}
+
+
+// add event listeners
 
 //1
+wordSearch.addEventListener("click",wordSearchName );
+anySearch.addEventListener("click",anySearchName );
+filter.addEventListener("click",filterName);
+search.addEventListener("change",searchByWord);
 
-btnPopulation.addEventListener("click",showPopulation)
-btnLanguage.addEventListener("click",showLanguage)
+
+
+
+
